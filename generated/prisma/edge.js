10470bd41ce82bb51273397ad6d5987dd35331b2
jeet-dot-dev <jeet999dev@gmail.com>
2025-07-04 18:18:31 +0530

    update gitignore

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -185,7 +185,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
